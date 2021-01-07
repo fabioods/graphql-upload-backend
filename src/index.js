2207d3a8590f9abca047cd9f2dc4a8e07721688e
Fábio Santos <fabio.santos@fundacaoabc.org>
2021-01-07 17:53:12 -0300

    correção do nome mimeType para mimetype

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import { ApolloServer, gql } from 'apollo-server';
+import { gql } from 'apollo-server';
+import { ApolloServer } from 'apollo-server-express';
 import fs from 'fs';
 import path from 'path';
+import cors from 'cors';
+import express from 'express';
+import bytea from 'postgres-bytea';
 
 const typeDefs = gql`
   type File {
     filename: String!
-    mimeType: String!
+    mimetype: String!
     encoding: String!
   }
 
@@ -14,25 +18,27 @@ const typeDefs = gql`
   }
 
   type Mutation {
-    singleUpload(file: Upload): File!
+    singleUpload(file: Upload!): File!
   }
 `;
 
 const resolvers = {
   Mutation: {
-    singleUpload: (parent, args) =>
-      args.file.then(file => {
-        const { createReadStream, filename, mimeType } = file;
+    singleUpload: (parent, args) => {
+      console.log('args', args);
+      return args.file.then(file => {
+        const { createReadStream, filename, mimetype } = file;
         const fileStream = createReadStream();
         const pathToUpload = path.resolve(__dirname, '..', 'upload');
         fileStream.pipe(fs.createWriteStream(`${pathToUpload}/${filename}`));
+        const b = fileStream.pipe(new bytea.Encoder());
         return file;
-      }),
+      });
+    },
   },
 };
-
+const app = express();
+app.use(cors());
 const server = new ApolloServer({ resolvers, typeDefs });
-
-server.listen().then(({ url }) => {
-  console.log(`Server running at ${url}`);
-});
+server.applyMiddleware({ app });
+app.listen(4000, () => console.log('Serviço rodando na porta 4000'));
